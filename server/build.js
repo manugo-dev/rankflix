aa5f3790ef0841083164da7b30469d22d89195ab
manugo-dev <manugo.dev@gmail.com>
2025-10-08 14:19:22 +0200

    chore: fix outfile destination for ssr-server

--- server/build.js
+++ server/build.js
@@ -3,7 +3,7 @@ import { build } from "esbuild";
 build({
   logLevel: "info",
   entryPoints: ["server/index.js"],
-  outfile: "/dist/index.js",
+  outfile: "dist/index.js",
   platform: "node",
   target: "node22",
   format: "esm",
