c922ef559fda88f912046a1a7f50b64b6cc42b4e
manugo-dev <manugo.dev@gmail.com>
2025-10-16 14:50:31 +0200

    chore: move movie features directly to features folder

--- src/features/discovery-movies/index.ts
+++ src/features/discovery-movies/index.ts
@@ -1,3 +1,4 @@
 export { discoverMoviesQueries } from "./api/queries";
 export { GenresSection } from "./ui/genres-section";
+export { SimilarMovies } from "./ui/similar-movies";
 export { TrendingSection } from "./ui/trending-section";
